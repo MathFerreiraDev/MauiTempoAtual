48083b1691295a428c44570ff4a9c14412317c00
Usuario <Usuario@CPD03-M18>
2024-07-29 12:14:57 -0300

    Adicionar arquivos de projeto.

---
+++ MauiTempoAtual/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiTempoAtual
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiTempoAtual/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiTempoAtual
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
